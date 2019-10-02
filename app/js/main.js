6399d61716f02502824517e84e1d50b6319d0b98
Le8wa-dev <savinchev@list.ru>
2019-10-02 11:57:05 +0300

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,15 @@ $(function(){
         $('.icon-th-list').removeClass('active')
     });
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
     });
@@ -42,6 +51,7 @@ $(function(){
     $('.header__btn-menu').on('click', function(){
         $('.header__box').toggleClass('active');
     });
+    
 
     var mixer = mixitup('.products__inner-box');
 
